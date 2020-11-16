61c543085b4466100f548e41ec27937621d6dc8a
jbt <jbt@303_INS>
2020-11-16 10:25:04 +0200

    aaa

---
+++ Lesson01/src/a/PersonTest.java
@@ -0,0 +1,12 @@
+package a;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+
+		Person p1 = new Person(111, "a", -1);
+		System.out.println(p1.getId() + ", " + p1.getName() + ", " + p1.getAge());
+
+	}
+
+}
