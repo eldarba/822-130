c3ac886706e71dcf7ba1f8649e7cdd02fde5615f
jbt <jbt@303_INS>
2020-11-16 13:04:15 +0200

    aaa

--- Lesson01/src/e/ctors/Person.java
+++ Lesson01/src/e/ctors/Person.java
@@ -2,4 +2,28 @@ package e.ctors;
 
 public class Person {
 
+	private int id;
+	private String name;
+	private int age;
+
+	public Person() {
+	}
+
+	public Person(int id, String name, int age) {
+		super();// invoke the CTOR of supr (Object class)
+		Person.this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public Person(int id) {
+		this(id, "default_name", 18);// invoke a CTOR in this class
+	}
+
+	public static void main(String[] args) {
+		System.out.println();
+		Person p1 = new Person(111, "aaa", 22);
+
+	}
+
 }
---
+++ Lesson01/src/e/ctors/PointTest.java
@@ -0,0 +1,12 @@
+package e.ctors;
+
+public class PointTest {
+
+	public static void main(String[] args) {
+		Point p1 = new Point(2, 4);
+
+		Pixel px1 = new Pixel();
+		System.out.println(px1.getX());
+
+	}
+}
