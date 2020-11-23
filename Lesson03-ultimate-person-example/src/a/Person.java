56a03bdb222c28ab172f8985fe5b43a6e5684220
Eldar <eldarba@gmail.com>
2020-11-23 14:32:49 +0200

    aaa

---
+++ Lesson03-ultimate-person-example/src/a/Person.java
@@ -0,0 +1,75 @@
+package a;
+
+public class Person {
+
+	private final int id;
+	private String name;
+	private int age;
+
+	// use eclipse to generate
+	/*
+	 * 1. CTOR
+	 * 
+	 * 2. get/set
+	 * 
+	 * 3. toString
+	 * 
+	 * 4. hashCode and equals
+	 */
+
+	public Person(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Person)) {
+			return false;
+		}
+		Person other = (Person) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ Lesson03-ultimate-person-example/src/a/Test1.java
@@ -0,0 +1,11 @@
+package a;
+
+public class Test1 {
+	public static void main(String[] args) {
+
+		Person p1 = new Person(101, "aaa", 22);
+		Person p2 = p1;
+
+		System.out.println(p1.equals(p2));
+	}
+}
