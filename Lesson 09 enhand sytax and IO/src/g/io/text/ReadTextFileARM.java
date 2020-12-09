28cb05cec86db4aae64a787c68bf8437012b34d5
Eldar <eldarba@gmail.com>
2020-12-09 16:20:10 +0200

    aaa

--- Lesson 09 enhand sytax and IO/src/g/io/text/ReadTextFileARM.java
+++ Lesson 09 enhand sytax and IO/src/g/io/text/ReadTextFileARM.java
@@ -4,31 +4,24 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadTextFile {
+public class ReadTextFileARM {
 
 	public static void main(String[] args) {
+
 		// 1. define a file object - source
 		File file = new File("files/letter.txt");
+
 		// 2. define a source object - IO Object
-		FileReader in = null;
-		try {
-			in = new FileReader(file);
+		try (FileReader in = new FileReader(file);) {
 			// 3. read
-			int c = in.read();
+			int c = in.read(); // read first char
 			while (c != -1) {
 				System.out.print((char) c);
-				c = in.read();
+				c = in.read(); // read next char
 			}
+			// 4. close the stream
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				// 4. close the stream
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
-
 }
--- Lesson 09 enhand sytax and IO/src/g/io/text/ReadTextFileBuffer.java
+++ Lesson 09 enhand sytax and IO/src/g/io/text/ReadTextFileBuffer.java
@@ -1,34 +1,28 @@
 package g.io.text;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadTextFile {
+public class ReadTextFileBuffer {
 
 	public static void main(String[] args) {
+
 		// 1. define a file object - source
 		File file = new File("files/letter.txt");
+
 		// 2. define a source object - IO Object
-		FileReader in = null;
-		try {
-			in = new FileReader(file);
+		try (BufferedReader in = new BufferedReader(new FileReader(file));) {
 			// 3. read
-			int c = in.read();
-			while (c != -1) {
-				System.out.print((char) c);
-				c = in.read();
+			String line = in.readLine(); // read first line
+			while (line != null) {
+				System.out.println(line);
+				line = in.readLine(); // read next line
 			}
+			// 4. close the stream
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				// 4. close the stream
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
-
 }
