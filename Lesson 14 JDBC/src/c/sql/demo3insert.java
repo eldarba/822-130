e53202831c22051e7342e3d6093366974e18d026
Eldar <eldarba@gmail.com>
2020-12-28 15:45:18 +0200

    aaa

--- Lesson 14 JDBC/src/c/sql/demo3insert.java
+++ Lesson 14 JDBC/src/c/sql/demo3insert.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class demo2CreateTable {
+public class demo3insert {
 
 	public static void main(String[] args) {
 
@@ -13,29 +13,29 @@ public class demo2CreateTable {
 		String user = "eldar1"; // use your own user
 		String password = "pass1"; // use your own password
 
-		String sql = "create table book";
-		sql += "(";
-		sql += "id int primary key, ";
-		sql += "title varchar(25), ";
-		sql += "author varchar(25), ";
-		sql += "price float, ";
-		sql += "publication date";
-		sql += ")";
-
 		try (Connection con = DriverManager.getConnection(url, user, password);) {
 			System.out.println("connected to: " + url);
 
 			// ===================work with the database======================
-
 			Statement stmt = con.createStatement();
-			stmt.executeUpdate(sql);
+			for (int i = 1; i <= 20; i++) {
 
-			System.out.println(sql);
+				String sql = "insert into book values(";
+				sql += i + ", ";
+				sql += "'title" + i + "', ";
+				sql += "'author" + i + "', ";
+				int r = (int) (Math.random() * 100);
+				sql += r + ", ";
+				sql += "'2020-12-28'";
+				sql += ")";
+				System.out.println(sql);
+				stmt.executeUpdate(sql);
+
+			}
+
 			// ===============================================================
 
 		} catch (SQLException e) {
-			System.out.println("failed connected to: " + url);
-			System.out.println(sql);
 			e.printStackTrace();
 		}
 
