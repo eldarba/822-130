7f47b66aa6b4fe4a0eddf797bcdedb2bb2f72096
jbt <jbt@host.docker.internal>
2021-04-28 16:28:40 +0300

    micro-services lesson 1

---
+++ service-a/src/main/java/app/core/web/ControllerA.java
@@ -0,0 +1,13 @@
+package app.core.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerA {
+
+	@GetMapping("/service/a")
+	public String handleA() {
+		return "Service A";
+	}
+}
